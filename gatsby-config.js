010e02d0906f48728dc2f3c82bb71456ff4ba5cd
Afiq Xilantra Azmi <artxlave@gmail.com>
2021-01-05 03:38:41 +0800

    Configure seo and meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,8 @@
+
+const settings = require("./src/util/site.json")
+
 module.exports = {
-  siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
-    description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
-  },
+  siteMetadata: settings.meta,
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
@@ -48,6 +47,9 @@ module.exports = {
               // the content container as this plugin uses this as the
               // base for generating different widths of each image.
               maxWidth: 2048,
+              linkImagesToOriginal: false,
+              tracedSVG: true,
+              loading: "lazy",
             },
           },
           {
@@ -59,6 +61,48 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: settings.ga,
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Enables Google Optimize Experiment ID
+        // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        // Set Variation ID. 0 for original 1,2,3....
+        // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: settings.siteUrl,
+      },
+    },
+    `gatsby-plugin-advanced-sitemap`,
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: settings.meta.title,
+    //     short_name: settings.meta.title,
+    //     start_url: `/`,
+    //     background_color: settings.meta.bgColor,
+    //     theme_color: settings.meta.themeColor,
+    //     display: `standalone`,
+    //     icon: "static" + settings.meta.iconimage,
+    //   },
+    // },
     {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
