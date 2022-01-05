f0ac35ae5a5c16fba80cc496c05ed3f3632e7d7f
Afiq Xilantra <artxlave@gmail.com>
2022-01-05 11:12:08 +0800

    Fix socialMedia component in footer

--- src/components/socialMedia.js
+++ src/components/socialMedia.js
@@ -1,16 +1,28 @@
-import * as React from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-function AccountList({ userName, accountUrl, iconName }) {
-    return (
-        <li className="column is-12">
-            <a title={userName} href={accountUrl} target="_blank" rel="noopener noreferrer">
-                <img
-                    src={iconName}
-                    alt={userName}
-                    style={{ width: "1em", height: "1em" }}
-                />
-            </a>
-        </li>
-    );
-  }
-export default AccountList;
+export default function AccountList(props) {
+  const {
+    userName,
+    accountUrl,
+    iconName
+  } = props;
+
+  return (
+    <li className="column is-12">
+        <a title={userName} href={accountUrl} target="_blank" rel="noopener noreferrer">
+            <img
+                src={iconName}
+                alt={userName}
+                style={{ width: "1em", height: "1em" }}
+            />
+        </a>
+    </li>
+  );
+}
+
+AccountList.propTypes = {
+    iconName: PropTypes.string,
+    userName: PropTypes.string,
+    accountUrl: PropTypes.string,
+};
